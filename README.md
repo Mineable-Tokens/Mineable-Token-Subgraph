6fc1b8fbea4e6b10e63038edcabd18cad3e1d488
andy <admazzola@gmail.com>
2021-01-28 15:14:21 -0500

    improving

---
+++ README.md
@@ -0,0 +1,12 @@
+#  Mineable Token Subgraph
+
+
+https://thegraph.com/docs/define-a-subgraph#data-source-templates
+
+
+## Running a Graph Node  (https://thegraph.com/docs/quick-start#local-development)
+
+git clone https://github.com/graphprotocol/graph-node/
+cd graph-node/docker
+./setup.sh
+docker-compose up
--- src/mapping.ts
+++ src/mapping.ts
@@ -6,31 +6,42 @@ import {
   Transfer,
   Approval
 } from "../generated/_0xBitcoinToken/_0xBitcoinToken"
-import { ExampleEntity } from "../generated/schema"
+import { MintCheckpoint } from "../generated/schema"
 
 export function handleMint(event: Mint): void {
+
+  let _0xBitcoincontract = _0xBitcoinToken.bind(event.address)
+
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = MintCheckpoint.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new MintCheckpoint(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.hashrate = 0
   }
 
-  // BigInt and BigDecimal math are supported
+
+  entity.difficulty = _0xBitcoincontract.miningTarget()
+
+
+/*  // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
   entity.from = event.params.from
-  entity.reward_amount = event.params.reward_amount
+  entity.reward_amount = event.params.reward_amount*/
 
   // Entities can be written to the store with `.save()`
   entity.save()
+
+
+
+
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
